b3e69581876b69cb3ad42d56ea735ae7d3fcca04
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-13 17:14:58 +0100

    change old file and add new file string.js

---
+++ string.js
@@ -0,0 +1,14 @@
+ 
+
+ let zmienna1 = "tekst1";
+ let zmienna2 = "tekst2";
+
+ let zmienna3 = zmienna1 + " " + zmienna2;
+
+ console.log(zmienna3);
+
+
+ let losoweLitery = 'ABCDEFG';
+
+ //zwraca nam ilość znaków ktore są w naszym stringu.
+ console.log(losoweLitery.length);
