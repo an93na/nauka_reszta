c16dd37ef6a04c8ac9d261ea61edd6fc595afa22
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-13 12:56:38 +0100

    add changes

---
+++ test.js
@@ -0,0 +1,19 @@
+console.log('dzialam');
+console.warn('działam');
+console.error('dzialam'); 
+
+let variable;
+let variable2;
+variable = 100;
+variable2 = 40;
+console.log(variable);
+console.log(`Value of variable after initialization is: ${variable}`);
+console.log(variable2);
+console.log(`Value of variable after initialization is: ${variable2}`);
+
+let number1 = 0;
+let number2 = 20;
+let number3 = -99;
+let number4 = 2988732.874327;
+
+console.log(`Value of number1 is: ${number1} and it's type is: ${typeof number1}`);
