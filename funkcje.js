4693b9d6ec81ef3dec5b060718b615d8668c5271
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-13 20:13:48 +0100

    next changes

--- funkcje.js
+++ funkcje.js
@@ -1,4 +1,4 @@
-
+console.log('~~~~~~Funkcja wyświetlająca mnożenie jakiejś liczby razy 2~~~~~~');
 
 function pierwszaFunkcja(a) {
     return a*2;
@@ -10,6 +10,8 @@ console.log(pierwszaFunkcja(2));
 console.log(pierwszaFunkcja(6));
 
 
+
+console.log(`~~~~~~~~~~~~~Funkcja witająca się ~~~~~~~~~~~~~~`);
 function drugaFunkcja() {
     return console.log('Hello')    
 }
@@ -18,6 +20,7 @@ drugaFunkcja();
 drugaFunkcja();
 drugaFunkcja();
 
+console.log(`~~~~~~~~~~~~~Funkcja witająca się z danym użytkownikiem~~~~~~~~`);
 function trzeciaFunkcja(b) {
     return console.log(`Cześć ${imie}!`);
 }
@@ -28,3 +31,16 @@ imie = 'Kasia'
 trzeciaFunkcja(imie);
 imie = 'Janek'
 trzeciaFunkcja(imie);
+
+
+// let bok1 = 5;
+// let bok2 = 3;
+console.log(`~~~~~~~~~~~~~Funkcja matematyczna~~~~~~~~~~`)
+
+function matematyczna(a, b) {
+    return console.log(`Pole prostokąta to: `, 2*a+2*b);
+}
+
+matematyczna(3,4);
+matematyczna(2,7);
+matematyczna(1,6);
