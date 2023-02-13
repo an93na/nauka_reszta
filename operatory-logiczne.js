a51318d230f7eda7fc644c179be1bc86f19f79e3
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-13 15:50:37 +0100

    changes

---
+++ operatory-logiczne.js
@@ -0,0 +1,17 @@
+// === dokładnie równy
+
+zmienna = '12';
+zmienna2 = 122;
+
+// Number.parseInt() zamienia string na liczbę
+Number.parseInt(zmienna);
+
+console.log('zmienna = ', zmienna);
+console.log(Number.parseInt(zmienna));
+
+console.log('zmienna2 = ', zmienna2);
+console.log('zmienna > 8: ', zmienna > 8);
+console.log('zmienna2 > 8: ', zmienna2 > 8);
+console.log('zmienna2 === 8: ', zmienna2 === 8);
+console.log('zmienna2 >= 8: ', zmienna2 >= 8);
+console.log('zmienna2 !== 8', zmienna2 !== 8);
