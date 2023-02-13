1116eb9ef77229b27c06473696127ac446647ac4
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-13 15:07:56 +0100

    changes

---
+++ operatory.js
@@ -0,0 +1,26 @@
+// znaki które można używać czyli + / * itd
+
+let num1 = 6;
+let num2 = 7;
+let num3 = num1 + num2;
+let num4 = (num3 * num2) - num1;
+
+let num5 = 5;
+console.log(`wartość num5: ` + num5);
+num5 += 7;
+console.log('wartość num5 po operacji +=7 : ' + num5);
+
+console.log('wartość num3: ' + num3);
+console.log(`wartość num4: ` + num4);
+console.log('wartość num4 /3: ' + num4 / 3);
+
+num4 -= 6;
+console.log('wartość num4 po operacji -=6 : ' + num4);
+
+num4 *=7
+console.log('wartość num4 po operacji *=7 : ' + num4);
+
+let i=1;
+i++; 
+
+console.log('wartość i po operacji i++ : ' + i);
