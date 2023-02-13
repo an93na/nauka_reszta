1a326227a7a997363fbbe7a039c69bd19e5f8bde
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-13 14:10:44 +0100

    changes file test.js and add new file zmienne.js

--- test.js
+++ test.js
@@ -1,19 +1,33 @@
-console.log('dzialam');
-console.warn('działam');
-console.error('dzialam'); 
+//wyswietlanie console.log i deklaracja zmiennych
 
-let variable;
-let variable2;
-variable = 100;
-variable2 = 40;
-console.log(variable);
-console.log(`Value of variable after initialization is: ${variable}`);
-console.log(variable2);
-console.log(`Value of variable after initialization is: ${variable2}`);
+console.log('Hello');
+console.warn('Word');
+console.error('Czy jakos tak :D'); 
+
+let zmienna;
+let zmienna2;
+zmienna = 120;
+zmienna2 = 80;
+console.log(zmienna);
+console.log(`Value of zmienna after initialization is: ${zmienna}`);
+console.log(zmienna2);
+console.log(`Value of zmienna after initialization is: ${zmienna2}`);
 
 let number1 = 0;
-let number2 = 20;
-let number3 = -99;
-let number4 = 2988732.874327;
+let number2 = 220;
+let number3 = -9;
+let number4 = 3488732.874327;
 
 console.log(`Value of number1 is: ${number1} and it's type is: ${typeof number1}`);
+
+let string1 = `It's my first day with JavaScript`;
+console.log(`Value of string1 is: ${string1} and it's type is: ${typeof string1}`);
+
+let PeopleLikeCandies = true;
+let sixIsAPrimeNumber1 = false;
+
+// const deklarujemy pewną stałą, let po prostu deklarujemy zmienną
+const PeopleLikeCandies2 = true;
+const sixIsAPrimeNumber2 = false;
+
+
---
+++ zmienne.js
@@ -0,0 +1,5 @@
+let imie = 'Kasia';
+let nazwisko = 'Nowak';
+let wiek = 29;
+let wyksztalceniewyzsze = true;
+
