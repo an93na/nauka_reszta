56424dc78dc85c6b6b10a4aadb39f9c124256e3e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-13 18:21:54 +0100

    changes

--- petle.js
+++ petle.js
@@ -20,6 +20,7 @@ else {
 }
 
 
+
 let wiek = 20;
 const pelnoletnioscPL = 18;
 const pelnoletnioscUS = 21;
@@ -34,6 +35,8 @@ else if (pelnoletnioscPL<wiek && pelnoletnioscUS<wiek) {
     console.log('You have enought age in PL & US');
 }
 
+
+
 let czekolada = 'mleczna';
 // let czekolada2 = 'mleczna';
 // let czekolada3 = 'gorzka';
@@ -45,4 +48,38 @@ else if (czekolada === 'gorzka') {
     console.log(`Ehh I sorry I don't like this type of chocolate :( `);    
 } else {
     console.log('Thank you');
+}
+
+
+let a = 40;
+
+if (3 > 9) {
+    a +=10
+}
+else {
+    a -=10
+}
+console.log(a);
+
+//skrot do petli if i wyjdzie true lub false i zapisuje odpowiedni tekst.
+let napis = (a > 50) ? 'Wieksze' : 'Mniejsze';
+
+console.log(napis);
+
+
+let aaa = 0;
+
+while (aaa <=40) {
+    console.log('mniejsze');
+
+    // if (aaa% 5 === 0) {
+    //     aaa +=6;
+    // }
+    // else if (aaa % 4 === 1) {
+    //     aaa +=7;
+    // }
+    // else {
+        aaa++
+        console.log(aaa);
+    // }
 }
