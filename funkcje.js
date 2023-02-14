6404578181d22a469be9304aa3ad18e1e32c4210
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-14 10:17:02 +0100

    changes

--- funkcje.js
+++ funkcje.js
@@ -49,12 +49,14 @@ matematyczna(1,6);
 console.log(`~~~~~~~~~~~~~Funkcja sprawdzająca~~~~~~~~~~`);
 
 function sprawdz(a) {
-    return console.log(a>10);
+    console.log(a>10);
+    return a
 }
 
 sprawdz(3);
 sprawdz(36);
 sprawdz(5);
+sprawdz(12);
 
 
 function podzielnosc(x) {
@@ -64,8 +66,50 @@ function podzielnosc(x) {
    else {
     console.log('liczba nie jest podzielna przez 3 bez reszty. Ta reszta wynosi', (x % 3));
    }
+   return 
 }
 
 podzielnosc(2);
 podzielnosc(7);
 podzielnosc(9);
+podzielnosc(12);
+console.log(`~~~~~~~~~~~~~~~~~`);
+
+liczba = 21;
+let arg1 = sprawdz(liczba);
+let arg2 = podzielnosc(liczba);
+// let arg1 = 16;
+// let arg2 = 12;
+
+if (arg1 > 10 && arg2 % 3 === 000) {
+    console.log('Git');
+}
+else {
+    console.log('Niestety nie');
+}
+
+let arg4 = 16;
+let arg5 = 12;
+
+if (arg4 > 10 && arg5 % 3 === 0) {
+    console.log('Git');
+}
+else {
+    console.log('Niestety nie');
+}
+
+console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+
+let euro = 4;
+let dolar = 3.5;
+
+function kurs(a, b) {
+    if (a > b) {
+        console.log(`Więcej kosztuje euro`);
+    }
+    else if (a < b) {
+        console.log(`Więcej kosztuje dolar`);
+    }
+}
+
+kurs(euro, dolar);
