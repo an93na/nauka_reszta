106d07c6cc9cffc530d7b6b359d57c858de21265
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-15 19:04:23 +0100

    changes

--- klasy.js
+++ klasy.js
@@ -53,3 +53,81 @@ tablicaKotow[0].przedstawSie();
 tablicaKotow[0].dajGlos();   
 tablicaKotow[1].przedstawSie();   
 tablicaKotow[1].dajGlos();   
+
+console.log('~~~~~~~~~~~~~~~Klasy~~~~~~~~~~~~~~~~~~~');
+
+class cat {
+    constructor (imie, wiek, rasa, waga) {
+        // console.log('konstruktor');
+        this.imieKota = imie;
+        this.wiekKota = wiek;
+        this.rasaKota = rasa;
+        this.wagaKota = waga;
+        this.najedzony = false;
+        this.szczesliwy = true;
+    }
+    zamiauczProsze () {
+        console.log (`nazywam się ${this.imieKota} i sobie miaucze: Miau Miau!`)
+    }
+
+
+
+czyJestesGlodny() {
+    if (this.najedzony === true) {
+        console.log('tak');
+    }
+    else {
+        console.log('o nie zjadłbym coś');
+    }
+}
+zjedz() {
+ this.najedzony = true;
+}
+
+bawSie() {
+    this.szczesliwy = true;
+    this.najedzony = false;
+    console.log(`tup tup tup yhu ale frajda`);
+}
+
+tesknijZaOpiekunem() {  
+    this.szczesliwy = false;
+    this.najedzony = false;
+    console.log(`smutno mi`)
+}
+odpoczywaj () {
+    console.log(`ZZzzz ZZZZzzz zzZZZz`)
+}
+}
+
+let mojKot = new cat('Amancik', 12, 'europejski', 6);
+console.log(mojKot.imieKota,mojKot.wiekKota,mojKot.rasaKota, mojKot.wagaKota);
+
+let tablicaKot = [
+    new cat('Amancik', 1, 'europejski', 5),
+    new cat('Tornado', 4, 'perski', 7),
+    new cat('Balascik', 2, 'syjamski', 6),
+    new cat('Pchełka', 6, 'leśny', 4)
+
+]
+
+tablicaKot[2].zamiauczProsze();
+console.log(`~~~~~~~~~~~~~~~~~~~~~`);
+
+let nowyKot = new cat('Kitku', 5, 'leśny', 5);
+
+if (nowyKot.czyJestesGlodny()) {
+    nowyKot.zjedz();
+} else {
+    nowyKot.bawSie();
+}
+
+nowyKot.tesknijZaOpiekunem();
+
+if (nowyKot.czyJestesGlodny()) {
+    nowyKot.zjedz();
+} else {
+    nowyKot.odpoczywaj()
+}
+
+console.log('~~~~~~~~~dziedziczenie~~~~~~~~~~~~~');
