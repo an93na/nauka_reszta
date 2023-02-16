5a8804dae29754f5907f7c5a14dc003b7cb8221a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-16 16:21:22 +0100

    changes

--- zamianaLnaA.js
+++ zamianaLnaA.js
@@ -35,8 +35,42 @@ function Potegowanie(podstawa, wykładnik) {
             
     } }
     console.log(wynik);
+    return wynik
 }
 
 Potegowanie(3,0);  
 Potegowanie(2,5);  console.log('sprawdzam', 2**5);  
 Potegowanie(4,3);  console.log('sprawdzam', 4**3);
+
+console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+
+// Funkcja sklejająca tablice 
+
+let a = [2, 3, 4];
+let b = [3, 4, 5, 6, 7];
+
+console.log('Sklejanie za pomocą .concat', a.concat(b));
+
+function sklejTab(podstawa, dodaj) {
+    let dodajIndex = 0;
+    let stop = podstawa.length+dodaj.length
+    for (let index =podstawa.length; index < stop; index++){
+        podstawa[index] = dodaj[dodajIndex];
+        dodajIndex++;
+    }
+    console.log(podstawa);
+    return podstawa;
+}
+
+let toWychodzi = sklejTab(a,b);
+sklejTab([1, 2],[2, 4, 5]);
+sklejTab([3, 2, 5],[7, 4, 5, 8, 9]);
+sklejTab([1, 2],[6, 7, 8, 2, 4, 5]);
+
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+
+ciagZnakow = 'ABCDEFGHIJKLMNOPRSTUQWXYZ'
+ let wycianij = ciagZnakow.substring(2, 10);
+ console.log(wycianij);
+ 
+ 
