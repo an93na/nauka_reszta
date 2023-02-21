232340cad8c6c57461bb09c2c3c3f58341f27036
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-21 18:31:26 +0100

    changes

--- zadania3.js
+++ zadania3.js
@@ -1,3 +1,5 @@
+console.log('~~~~~~~~~~~~~~~~~~~~1~~~~~~~~~~~~~~~~~~~~');
+
 //1. Łączenie 3 stringów w jeden
 
 let zmienna1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
@@ -7,7 +9,7 @@ let zmienna3 = 'neque eum velit esse aliquid! Ab perspiciatis ex deleniti!'
 zad1 = zmienna1+zmienna2+zmienna3;
 console.log(zad1);
 
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~2~~~~~~~~~~~~~~~~~~~~');
 
 //2. Sprawdzanie długości zmiennej z poprzedniego zadania.
 
@@ -16,12 +18,27 @@ zad2 = zad1.length
 console.log(zad2);
 
 // za pomocą funkcji
-function sprawdz2 (a){
+function sprawdz2(a){
     let wynik
-    for(let i=0; i<a.length ;i++){
+    for(let i=1; i<a.length ;i++){
         wynik = i
     }
     return wynik
 }
 
 console.log(sprawdz2(zad1));
+
+console.log('~~~~~~~~~~~~~~~~~~~~3~~~~~~~~~~~~~~~~~~~~');
+//3. Wyświetlanie 10 kolejnych liter zaczynając od 7
+
+function zad3(a,b,c){
+    let wynik
+    for(let i=b; i<=b+c; i++){
+        wynik = i;
+    }
+    console.log(wynik)
+    a = a.slice(b, wynik)
+    return a
+}
+
+console.log(zad3(zad1,5,10));
