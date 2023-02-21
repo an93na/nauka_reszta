5565d1a7632b00576c5dd40e921aac7bf3084b96
Anna Giermakowska <an.giermakowska@gmail.com>
2023-02-21 18:07:53 +0100

    changes

---
+++ zadania3.js
@@ -0,0 +1,8 @@
+//1. Łączenie 3 stringów w jeden
+
+let zmienna1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+let zmienna2 = 'Ex pariatur odit repellat animi ab? Vitae fuga tempore ratione illo nihil qui,'
+let zmienna3 = 'neque eum velit esse aliquid! Ab perspiciatis ex deleniti!'
+
+zad1 = zmienna1+zmienna2+zmienna3;
+console.log(zad1);
