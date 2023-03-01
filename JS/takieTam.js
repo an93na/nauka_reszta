3f58bb87d4406442321e896ea2f73b1449ddb694
Anna Giermakowska <an.giermakowska@gmail.com>
2023-03-01 19:51:55 +0100

    changes

--- JS/takieTam.js
+++ JS/takieTam.js
@@ -9,3 +9,12 @@ let tab2 = [...txt];
 console.log(tab2);
 
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+const tab = ["Marcin", "Ania", 'Agata',"Agnieszka", "Monika"];
+
+if (tab.includes("Agata")) {
+    console.log("Agata występuje w tablicy pod indeksem", tab.indexOf("Agata"));
+}
+
+if (!tab.includes("Kurczak")) {
+    console.log("Kurczak nie występuje w tej tablicy");
+}
