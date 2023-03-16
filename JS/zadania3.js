2741f4eaa54d2193983a2871488457838f563f81
Anna Giermakowska <an.giermakowska@gmail.com>
2023-03-16 20:02:10 +0100

    changes

--- JS/zadania3.js
+++ JS/zadania3.js
@@ -6,7 +6,7 @@ let zmienna1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
 let zmienna2 = 'Ex pariatur odit repellat animi ab? Vitae fuga tempore ratione illo nihil qui,'
 let zmienna3 = 'neque eum velit esse aliquid! Ab perspiciatis ex deleniti!'
 
-zad1 = zmienna1+zmienna2+zmienna3;
+let zad1 = zmienna1+zmienna2+zmienna3;
 console.log('Całe zdanie po połączeniu: ',zad1);
 
 console.log('~~~~~~~~~~~~~~~~~~~~2~~~~~~~~~~~~~~~~~~~~');
@@ -14,7 +14,7 @@ console.log('~~~~~~~~~~~~~~~~~~~~2~~~~~~~~~~~~~~~~~~~~');
 //2. Sprawdzanie długości zmiennej z poprzedniego zadania.
 
 // za pomocą length
-zad2 = zad1.length
+let zad2 = zad1.length
 console.log(zad2);
 
 // za pomocą funkcji
