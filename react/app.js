e692b13fb02c4e7186f9bd1b45f0ab0f48291753
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-01 17:42:50 +0200

    changes

--- react/app.js
+++ react/app.js
@@ -4,3 +4,18 @@ function getSquaredNumbers() {
     return numbers.map((a) => a*a);
   }
   console.log(getSquaredNumbers());
+
+
+  const phrases = ["react", "trelemorele", "frontend", "angular", "js", ""];
+
+function getLenghtsOfPhrases(){
+  let tab = [] 
+  phrases.forEach(p => {
+     tab.push(p.length);
+  })
+  // console.log(tab);
+  // console.log(phrases);
+  return tab;
+};
+const dlugoscStringow = getLenghtsOfPhrases();
+console.log(dlugoscStringow);
