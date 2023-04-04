7d468e1c046bfd20ed8e08836795a161578c2692
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-04 20:08:54 +0200

    changes

--- JS/zadania3.js
+++ JS/zadania3.js
@@ -318,20 +318,35 @@ input16.setAttribute('type', 'text');
 input16.setAttribute('placeholder','zamiana a,e,o na x');
 document.body.appendChild(input16);
 
+// let text16a = input16.addEventListener('input', ()=> {
+//     let wynik16a = '';
+//     let zamianaNaMaleLitery = input16.value.toLocaleLowerCase()
+//     // console.log(zamianaNaMaleLitery)
+//     for(let i = 0; i<input16.value.length; i++){
+//         if (zamianaNaMaleLitery[i]==='a' || zamianaNaMaleLitery[i] === "e" || zamianaNaMaleLitery[i] === 'o') {
+//             wynik16a+= 'x';
+//         }
+//         else {
+//             wynik16a+= zamianaNaMaleLitery[i];
+//         }
+// }
+//     console.log(wynik16a);
+// });
+
+// Za pomocą forEach
 let text16a = input16.addEventListener('input', ()=> {
     let wynik16a = '';
-    let zamianaNaMaleLitery = input16.value.toLocaleLowerCase()
-    // console.log(zamianaNaMaleLitery)
-    for(let i = 0; i<input16.value.length; i++){
-        if (zamianaNaMaleLitery[i]==='a' || zamianaNaMaleLitery[i] === "e" || zamianaNaMaleLitery[i] === 'o') {
-            wynik16a+= 'x';
+    let zamianaNaMaleLitery = input16.value.toLocaleLowerCase();
+    Array.from(zamianaNaMaleLitery).forEach((litera) => {
+        if (litera === 'a' || litera === 'e' || litera === 'o') {
+            wynik16a += 'x';
+        } else {
+            wynik16a += litera;
         }
-        else {
-            wynik16a+= zamianaNaMaleLitery[i];
-        }
-}
+    });
     console.log(wynik16a);
 });
+
 
 console.log('~~~~~~~~~~~~~~~~~~~~17~~~~~~~~~~~~~~~~~~~~');
 //17. złączanie 3 słów w jedno.
