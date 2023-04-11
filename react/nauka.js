d9156e8692c433c3a2e099d43cc772acf92c18bb
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-11 12:24:30 +0200

    changes

--- react/nauka.js
+++ react/nauka.js
@@ -1,1 +1,10 @@
-console.log('działa');
+// Zadanie 1 - usuń/odfiltruj wszystkie stringi z tablicy które mają mniej znaków niż 3
+
+const strings2 = ["adsfasdf", "qweqwdasd", "as", "assadwq", "s", "", 123];
+
+function filtruj(){
+   return strings2.filter((string)=> string.length <= 3)
+}
+
+const wiecejNiz3 = filtruj();
+console.log(wiecejNiz3);
