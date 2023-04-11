d6b0dabbfed830555b45bea57b9a9d3dab7b5c4a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-11 12:36:22 +0200

    changes

--- react/nauka.js
+++ react/nauka.js
@@ -2,9 +2,22 @@
 
 const strings2 = ["adsfasdf", "qweqwdasd", "as", "assadwq", "s", "", 123];
 
+// I sposób z funkcją filter
 function filtruj(){
-   return strings2.filter((string)=> string.length <= 3)
+   return strings2.filter((string)=> string.length > 3)
 }
 
 const wiecejNiz3 = filtruj();
-console.log(wiecejNiz3);
+console.table(wiecejNiz3);
+
+// II sposób z pętlą for 
+
+// const strings3 = [];
+
+// for(let i=0; i<strings2.length; i++){
+//     if(strings2[i]>3){
+//         strings3.push(strings2[i]);
+//     }
+// }
+// console.table(strings3)
+
