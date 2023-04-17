3881bda7bc0ccf34849e2d1763f42268d0a539c9
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-17 17:52:26 +0200

    changes

---
+++ react/app2.js
@@ -0,0 +1,22 @@
+// ZAD21
+
+const wpisy = [
+    {
+      title: "wpis 1",
+      subTitle: "podtytuł - wpis 1",
+      paragraph: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim odit
+      quidem repellat quam officia facere.`,
+    },
+    {
+      title: "wpis 2",
+      subTitle: "podtytuł - wpis 2",
+      paragraph: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim odit
+      quidem repellat quam officia facere.`,
+    },
+    {
+      title: "wpis 3",
+      subTitle: "podtytuł - wpis 3",
+      paragraph: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim odit
+      quidem repellat quam officia facere.`,
+    },
+  ];
