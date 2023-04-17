5fc491ac1607b10ac94872cf8c49b6eba8f7981d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-17 17:54:43 +0200

    changes

--- react/app2.js
+++ react/app2.js
@@ -20,3 +20,34 @@ const wpisy = [
       quidem repellat quam officia facere.`,
     },
   ];
+
+  // funkcja przyjmuje post i to będzie obiekt. Czyli możemy sobie przekazać te posty z góry przy pomocy pętli np map albo forEach
+const renderPost = (post) => {
+    // tworzymy html przy pomocy stringa
+    const html = `
+      <h2>${post.title}</h2> 
+      <h3>${post.subTitle}</h3>
+      <p>
+         ${post.paragraph}
+      </p>
+      <a role="a">Czytaj dalej...</a>
+      `;
+      // u góry stosujemy konkatynacje stringów i wrzucamy tam dane
+     
+    // tworzymy nowy element article
+    const article = document.createElement("article");
+  
+    // w tym obiekcie article umieszczamy albo odczytujemy kod html któey jest pomiędzy <article>...tu to ...</article>
+    article.innerHTML = html;
+  
+    // pobieramy istniejący element z pliku index.html. Wyciągamy element z main i klasy container. I wstrzykujemy te
+    // artykuły za <h1> ....
+    document.querySelector("main.container").appendChild(article);
+  };
+  
+  // Wywołanie naszej funkcji z postami
+  // renderPost(wpisy[0]);
+  // renderPost(wpisy[1]);
+  // renderPost(wpisy[2]);
+  
+  
