401fd1b09151fd7762bf255a2709db2519358f84
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-20 20:57:01 +0200

    changes

--- powtorka/js/proba.js
+++ powtorka/js/proba.js
@@ -1,3 +1,12 @@
 // ctr ~ otwiera nam terminal
 
-console.log('wyświetlam to co ("tu") w consoli')
+console.log('wyświetlam to co tu w consoli')
+
+// powielanie lini: lewy alt   shift   strzałka w dół
+
+/* komentarz blokowy lewy shift lewy alt A */
+
+// przenoszenie czegoś na dół w góre lewy alt i strzałka  
+
+// deklaracja zmiennej
+let zmienna = 150
