3f75782233cf48f6f66e30f7500b6fc15f8df72e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-21 18:21:40 +0200

    changes

--- powtorka/js/proba.js
+++ powtorka/js/proba.js
@@ -19,6 +19,7 @@ console.log("zmienna1: " + zmienna);
 console.log("zmienna1: ", zmienna);
 console.log(`zmienna2: ${zmienna2}`);
 
+// zmienne number
 let number1 = 0;
 let number2 = 20;
 let number3 = 88.232432;
@@ -26,4 +27,16 @@ let tekst = "tekst";
 
 console.log("Typ zmiennej number1 to: ", typeof number1);
 console.log("Typ zmiennej tekst to: ", typeof tekst);
+
+// zmienne string
+let string1 = "It's cool";
+// tutaj użyty jest escape character żeby ten apostrof wewnątrz
+// tego stringa był apostrofem a nie końcem lini 'It\'s cool'
+let string2 = "It's cool";
+
+// zmienne boolean
+const SunIsStar = true;
+const MoonIsStar = false;
+// let deklaracja zmiennej ale może być też const co oznacza
+// constans czyli coś jest stałe
 
