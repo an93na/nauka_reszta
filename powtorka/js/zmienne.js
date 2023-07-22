1ec4cc69732d1ebfad5993e96f71df512246d219
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-22 22:03:34 +0200

    changes

---
+++ powtorka/js/zmienne.js
@@ -0,0 +1,9 @@
+let name = "Dominika";
+let lastName = "Dominikana";
+let myAge = 23;
+let isAdult = true;
+
+let name2 = "Krzysztof";
+let lastName2 = "Kowalski";
+let myAge2 = 28;
+let isAdult2 = true;
