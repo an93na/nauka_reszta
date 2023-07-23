b337d36ed0fcc36131dde5b451ceb9c4152b292a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-23 22:36:15 +0200

    changes

--- powtorka/js/zmienne.js
+++ powtorka/js/zmienne.js
@@ -7,3 +7,14 @@ let name2 = "Krzysztof";
 let lastName2 = "Kowalski";
 let myAge2 = 28;
 let isAdult2 = true;
+
+// pusty objekt
+let obiekt = {}
+
+// obiekt z zawartością
+let personObject = {
+    name: 'Tadeusz',
+    lastName: 'Nowak',
+    age: 44,
+    isAdult: true
+}
