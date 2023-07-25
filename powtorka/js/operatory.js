e9c1756667bd4341856fbbe716c9814b906e52ab
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-25 10:53:11 +0200

    changes

--- powtorka/js/operatory.js
+++ powtorka/js/operatory.js
@@ -1,10 +1,10 @@
 let var4 = 5;
 console.log(var4);
 
-var4= 100 
+var4 = 100;
 console.log(var4);
 
-var4 += 43
+var4 += 43;
 /*  var4 = var4+43
 += -= *= /=
  */
@@ -12,10 +12,23 @@ console.log(var4);
 
 // ++
 let i = 4;
-i++
-/*  i++
+i++;
+/*  
+    i++
     i = i+1
     i=1+1
     i=2
 */
-console.log(i)
+console.log(i);
+
+console.log(`preInkremetacja`);
+let a = 1;
+console.log(a);
+console.log(a++);
+console.log(a);
+
+console.log(`postInkrementacja`);
+let b = 1;
+console.log(b);
+console.log(++b);
+console.log(b);
