bf0758e6131711cd5b2a6539eed765ade5b3bb80
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-26 14:37:22 +0200

    changes

--- powtorka/js/instrukcjeSterujace.js
+++ powtorka/js/instrukcjeSterujace.js
@@ -15,8 +15,23 @@ const pelnoletniWUSA = 21;
 
 if (wiek < pelnoletniWPolsce) {
   console.log("Nie jesteś pełnoletni ani w Polsce ani w USA");
-} else if (wiek > pelnoletniWPolsce && wiek < pelnoletniWUSA) {
+}
+// && mówi nam że oba warunki muszą być spełnione
+else if (wiek > pelnoletniWPolsce && wiek < pelnoletniWUSA) {
   console.log("Jesteś pełnoletni w Polsce ale nie jesteś pełnoletni w USA");
 } else {
   console.log("Jesteś pełnoletni w Polsce i w USA");
+}
+
+// wybór koloru niebieski zielony czerwony
+
+let kolor = "braz";
+// || ten lub inny 
+if (kolor === "czerwony" || kolor === "niebieski") {
+  console.log("To idealny wybór");
+} else if (kolor === "zielony") {
+  console.log("nie przepadam za tym kolorem");
+}
+else {
+    console.log('no może być')
 }
