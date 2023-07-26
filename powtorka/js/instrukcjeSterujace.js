9dbb2a7c5dcaead09ec1ed14b64bea10d3a48df8
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-26 14:14:44 +0200

    changes

--- powtorka/js/instrukcjeSterujace.js
+++ powtorka/js/instrukcjeSterujace.js
@@ -8,3 +8,15 @@ if (czyMoznaWykonac) {
 } else {
   console.log("Opreacja nie powiodła się. Sprawdź ilość dostępnych środków");
 }
+
+let wiek = 28;
+const pelnoletniWPolsce = 18;
+const pelnoletniWUSA = 21;
+
+if (wiek < pelnoletniWPolsce) {
+  console.log("Nie jesteś pełnoletni ani w Polsce ani w USA");
+} else if (wiek > pelnoletniWPolsce && wiek < pelnoletniWUSA) {
+  console.log("Jesteś pełnoletni w Polsce ale nie jesteś pełnoletni w USA");
+} else {
+  console.log("Jesteś pełnoletni w Polsce i w USA");
+}
