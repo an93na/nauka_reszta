99d2b75a2032ce50ffabe4c21028fb0bd73cacd4
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-26 14:00:53 +0200

    changes

--- powtorka/js/instrukcjeSterujace.js
+++ powtorka/js/instrukcjeSterujace.js
@@ -1,5 +1,10 @@
-
 let bilans = 1000;
 let debet = 400;
 
 let czyMoznaWykonac = bilans > debet;
+
+if (czyMoznaWykonac) {
+  console.log("Opreacja przebiegła pomyślnie");
+} else {
+  console.log("Opreacja nie powiodzła się. Sprawdź ilość dostępnych środków");
+}
