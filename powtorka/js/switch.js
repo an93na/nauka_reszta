d4e89e55e1a51bd8a7354ddf345abe3103437514
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-30 14:43:32 +0200

    changes

--- powtorka/js/switch.js
+++ powtorka/js/switch.js
@@ -1,22 +1,26 @@
-let pojazd = "rower"; // 'samochód' 'rower' 'tuktuk'
+let pojazd = "deskorolka"; // 'samochód' 'rower' 'tuktuk'
 let iloscKol;
 
+// swicht to instrukcja sterująca
 switch (pojazd) {
   case "samochód": {
-    console.log("samochód");
+    console.log(`Pojazd to: samochód`);
     iloscKol = 4;
+    // break przerywa wykonanie jeśli coś się zgadza to przerwa wykonanie
+    // inaczej wykona wyszystkie casy po kolei
     break;
   }
   case "rower": {
-    console.log("rower");
+    console.log("Pojazd to: rower");
     iloscKol = 2;
     break;
   }
   case "tuktuk": {
-    console.log("tuktuk");
+    console.log("Pojazd to: tuktuk");
     iloscKol = 3;
     break;
   }
+//   default to odpowiednik elsa tam trafiają wszystkie przypadki których nie ma w case
   default: {
     console.log(`typ ${pojazd} nie jest obsługiwany`);
     break;
