8d71cfecbc10a6c41517af92a4ae9ff675e2e59d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-30 14:48:31 +0200

    changes

--- powtorka/js/switch.js
+++ powtorka/js/switch.js
@@ -1,4 +1,4 @@
-let pojazd = "deskorolka"; // 'samochód' 'rower' 'tuktuk'
+let pojazd = "samochód"; // 'samochód' 'rower' 'tuktuk'
 let iloscKol;
 
 // swicht to instrukcja sterująca
@@ -20,10 +20,28 @@ switch (pojazd) {
     iloscKol = 3;
     break;
   }
-//   default to odpowiednik elsa tam trafiają wszystkie przypadki których nie ma w case
+  //   default to odpowiednik elsa tam trafiają wszystkie przypadki których nie ma w case
   default: {
     console.log(`typ ${pojazd} nie jest obsługiwany`);
     break;
   }
 }
-console.log(`ilość kół to: ${iloscKol}`)
+console.log(`ilość kół to: ${iloscKol}`);
+
+// to co wyżej ale jako if else
+if (pojazd === "samochód") {
+  console.log(`Pojazd to: samochód`);
+  iloscKol = 4;
+  console.log(`ilość kół to: ${iloscKol}`);
+} else if (pojazd === "rower") {
+  console.log("Pojazd to: rower");
+  iloscKol = 2;
+  console.log(`ilość kół to: ${iloscKol}`);
+} else if (pojazd === "tuktuk") {
+    console.log("Pojazd to: tuktuk");
+    iloscKol = 3;
+    console.log(`ilość kół to: ${iloscKol}`);
+} else {
+    console.log(`typ ${pojazd} nie jest obsługiwany`);
+    console.log(`ilość kół to: brak w bazie`);
+}
