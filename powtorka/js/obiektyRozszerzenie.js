7e13036ac344cd5991047924b7fa87647b149902
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-03 19:37:44 +0200

    changes

--- powtorka/js/obiektyRozszerzenie.js
+++ powtorka/js/obiektyRozszerzenie.js
@@ -14,12 +14,16 @@ console.log(imie, wybranaZmienna);
 // akcessor z kropka nie jest dynamiczny
 
 let randomowyObiekt = {
-    0: 'x0',
-    1: 'y1',
-    2: 'z2',
+    3: 'x0',
+    9: 'y1',
+    5: 'z2',
     normalnePole: 404
 }
 /* nazwa nie może zaczynać się od cyfry, a w obiekcie można tak wpisać,
 ale nie można się dobrać do pola przez zwykły zapis randomowyObiekt.0
-bo to nie działa, ale te cyfry pod spodem zamieniają się na stringi i wtedy to działa */
-console.log(randomowyObiekt[0])
+bo to nie działa, ale te cyfry pod spodem zamieniają się na stringi i wtedy to działa.*/
+console.log(randomowyObiekt[3])
+/* I tutaj jest różnica pomiędzy tablicą a obiektem. Bo w tablicy mamy indexy i 
+jeśli piszemy tab[3] to chodzi nam o wartośc pola z indexem 3. A w obiekcie jeśli piszemy 
+[3] to musi istnieć pole z taką etykietą, czyli z taka nazwą. Jesli nie ma takiego pola
+to dostaniemy undefined */
