48160b2f79c6f548557ecff6732612f709cd437b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-06 21:28:13 +0200

    changes

--- powtorka/js/klasy.js
+++ powtorka/js/klasy.js
@@ -14,7 +14,9 @@ let pies = {
   },
 };
 
-function funkcjaSzczeknij () {
+function funkcjaSzczeknij() {
+  // this czyli ten i jak wywołujemy tą funkcję na tablicy to wtedy nam się konkretny
+  // index podstwia z konkretnym psem (patrz od lini 59 - 65)
   if (this.kraj === "Polska") {
     console.log("Hau Hau ");
   } else if (this.kraj === "Anglia") {
@@ -22,11 +24,11 @@ function funkcjaSzczeknij () {
   } else {
     console.log("Nie umiem szczekać w tym kraju");
   }
-};
+}
 
 function funkcjaPrzedstawiajca() {
   console.log(`Hej jestem psem i mam na imię ${this.imie}`);
-};
+}
 
 let dogsArray = [
   {
@@ -62,3 +64,40 @@ dogsArray[0].przedstawSie();
 dogsArray[1].przedstawSie();
 dogsArray[2].przedstawSie();
 
+// klasa
+class Pies {
+  //  specjalna funkcja tworząca klasę
+  constructor(imie, wiek, rasa, waga) {
+    console.log("Wywołano konstruktor klasy PIES");
+    this.name = imie;
+    this.age = wiek;
+    this.rasa = rasa;
+    this.weight = waga;
+    this.najedzony = false;
+  }
+  //   metody
+  podajImieProsze() {
+    console.log(`Nazywam się ${this.name} i sobie szczekam`);
+  }
+  czyJestesGlodny() {}
+  zjedz() {}
+  biegaj() {}
+  tesknijZaOpiekunem() {}
+}
+
+let mojNowyPies = new Pies("Lusia", 6, "kundel", 12);
+console.log(
+  mojNowyPies.name,
+  mojNowyPies.age,
+  mojNowyPies.rasa,
+  mojNowyPies.weight
+);
+
+let tablicaPsow = [
+  new Pies("Lusia", 6, "kundel", 12),
+  new Pies("Lucek", 2, "york", 8),
+  new Pies("Felek", 11, "podhalanski", 35),
+  new Pies("Flora", 8, "pudel", 10),
+];
+
+tablicaPsow[2].podajImieProsze();
