796b98d7fc4383313cb9d7ed3a34423472a4a471
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-08 18:38:50 +0200

    changes

---
+++ powtorka/js/wyjatki.js
@@ -0,0 +1,34 @@
+let mainCustomer = {
+  name: "Jan",
+  surname: "Jakiś",
+  address: {
+    city: "NY",
+    zip: 888,
+    street: "5th Avenue",
+  },
+  logAddress: function () {
+    console.log(this.address);
+  },
+};
+
+function changeAddress(customer, newAddress) {
+  if (newAddress === undefined) {
+    console.error("Parametr newAddres jest wymagany");
+    // przerwie działanie funkicji i nie przypisze undefined zabezpieczyliśmy się przed tym
+    return;
+  }
+  if (
+    newAddress.city === undefined ||
+    newAddress.zip === undefined ||
+    newAddress.street === undefined
+  ) {
+    console.erorr("newAddress object nie jest kompletny");
+    return;
+  }
+  //   jesteśmy pewni że newAddress jest ok
+  customer.address = newAddress;
+}
+
+mainCustomer.logAddress();
+changeAddress(mainCustomer);
+mainCustomer.logAddress();
