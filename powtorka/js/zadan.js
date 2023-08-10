3238912e99dabd2e2850be36eddb1c53345f7ec8
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-10 20:39:39 +0200

    changes

--- powtorka/js/zadan.js
+++ powtorka/js/zadan.js
@@ -14,14 +14,17 @@ wywolanie2(3, 5);
 
 // potęgowanie 2**3 to znaczy 2 do potęgi 3
 // Pierwsza cyfra podzielna przez 123 w zakresie od 0 - 1000
-let wynikPodzielny
-for(idx = 1; idx <= 1000; idx++){
-  if(idx%123 === 0){
-    console.log('Wynik to: ');
-    wynikPodzielny = idx;
-  } else {
-    console.log('Niestety nie')
+let wynikPodzielny;
+for (idx = 1; idx <= 1000; idx++) {
+  if (idx % 123 === 0) {
+    console.log("Wynik to: ", idx);
+    if(wynikPodzielny===undefined){
+      wynikPodzielny = idx;
+    }
+    else {
+      console.log(idx + ' też jest podzielny przez 123 ale ja mam już wynik')
+    }
   }
 }
 
-console.log(wynikPodzielny)
+console.log(wynikPodzielny);
