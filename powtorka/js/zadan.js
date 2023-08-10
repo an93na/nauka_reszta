70b8878c94db9ef744002ecb93c5d0edb7808e0d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-10 20:03:05 +0200

    changes

---
+++ powtorka/js/zadan.js
@@ -0,0 +1,15 @@
+function wywolanie(n, start) {
+  for (let index = start; index < n + start; index++) {
+    console.log(index);
+  }
+}
+// wywolanie(2, 5);
+
+function wywolanie2(ile, start) {
+  for (let index = 0; index < ile; index++) {
+    console.log(start + index);
+  }
+}
+wywolanie2(3, 5);
+
+// potęgowanie 2**3 to znaczy 2 do potęgi 3
