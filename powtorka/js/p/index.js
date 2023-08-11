3ee9f9197c4d0ca0373851df5c56ab522c1330cb
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-11 10:15:17 +0200

    changes

--- powtorka/js/p/index.js
+++ powtorka/js/p/index.js
@@ -13,3 +13,13 @@ let dane = [
   new Ksiazka("Rosół", 40),
   new Ksiazka("Kuchnia Polska", 400),
 ];
+
+// znajdzie div z id main
+let div = document.getElementById("main");
+// początek listy
+let ul = document.createElement("ul");
+let li = document.createElement("li");
+// dodaje tekst
+li.innerHTML = "coś tam";
+div.appendChild(ul);
+ul.appendChild(li);
