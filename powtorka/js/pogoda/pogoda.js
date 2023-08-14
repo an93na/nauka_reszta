4011e66ba613dab3b5fe153f268d4a4051ec816d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-14 19:22:11 +0200

    changes

--- powtorka/js/pogoda/pogoda.js
+++ powtorka/js/pogoda/pogoda.js
@@ -1,23 +1,36 @@
-const api_key = "ac6428ea7fc0ef9caef037d08a02ce91";
-const miasto = "Lodz";
-const endpoint =
-  `https://api.openweathermap.org/data/2.5/weather?q=${miasto}&units=metric&lang=pl&appid=` +
-  api_key;
+function getApiData(miasto) {
+  const api_key = "ac6428ea7fc0ef9caef037d08a02ce91";
+  // const miasto = "Lodz";
+  const endpoint =
+    `https://api.openweathermap.org/data/2.5/weather?q=${miasto}&units=metric&lang=pl&appid=` +
+    api_key;
 
-fetch(endpoint)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data); // wyświetlanie danych w konsoli
-    const city = data.name;
-    const temp = data.main.temp;
-    const desc = data.weather[0].description;
-    const icon =
-      "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); // wyświetlanie danych w konsoli
+      const city = data.name;
+      const temp = data.main.temp;
+      const desc = data.weather[0].description;
+      const icon =
+        "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 
-    // wstawienie danych do HTML
-    document.getElementById("city").textContent = city;
-    document.getElementById("temp").textContent = temp + "°C";
-    document.getElementById("desc").textContent = desc;
-    document.getElementById("icon").src = icon;
-  })
-  .catch((error) => console.error(error));
+      // wstawienie danych do HTML
+      document.getElementById("city").textContent = city;
+      document.getElementById("temp").textContent = temp + "°C";
+      document.getElementById("desc").textContent = desc;
+      document.getElementById("icon").src = icon;
+    })
+    .catch((error) => console.error(error));
+}
+function submitForm(event) {
+  event.preventDefault();
+  // console.log(event);
+  const cityName = document.getElementById("cityName").value;
+  // console.log(cityName);
+  getApiData(cityName);
+}
+
+document.getElementById("formularz").addEventListener("submit", submitForm);
+// getApiData("Warszawa");
+getApiData(document.getElementById("cityName").value);
