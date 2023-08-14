dbb05b32267b4ed57c454e8e5a4065c8c3db1b35
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-14 18:33:37 +0200

    changes

--- powtorka/js/pogoda/pogoda.js
+++ powtorka/js/pogoda/pogoda.js
@@ -1,1 +1,22 @@
+const api_key = "ac6428ea7fc0ef9caef037d08a02ce91";
+const endpoint =
+  "https://api.openweathermap.org/data/2.5/weather?q=Warsaw&units=metric&lang=pl&appid=" +
+  api_key;
 
+fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data); // wyświetlanie danych w konsoli
+    const city = data.name;
+    const temp = data.main.temp;
+    const desc = data.weather[0].description;
+    const icon =
+      "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+    // wstawienie danych do HTML
+    document.getElementById("city").textContent = city;
+    document.getElementById("temp").textContent = temp + "C";
+    document.getElementById("desc").textContent = desc;
+    document.getElementById("icon").src = icon;
+  })
+  .catch((error) => console.error(error));
