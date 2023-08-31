aaae4ccfd17ee28fc837d76c8c3a4f97b8725d95
Anna Giermakowska <an.giermakowska@gmail.com>
2023-08-31 20:31:06 +0200

    changes

--- powtorka/dom/index.js
+++ powtorka/dom/index.js
@@ -1,7 +1,13 @@
 // DOM - model obiektowy dokumentu, struktura drzewiasta.
 
-let elementyTd = document.querySelectorAll("td");
-let pierwszyElementTd = elementyTd[0];
-let ostatniElementTd = elementyTd[elementyTd.length - 1];
+const elementyTd = document.querySelectorAll("td");
+const pierwszyElementTd = elementyTd[0];
+const ostatniElementTd = elementyTd[elementyTd.length - 1];
 
 console.log("Dokument zawiera ", elementyTd.length + " elementy <td>");
+console.log("Pierwszy element to: ", pierwszyElementTd);
+console.log("Ostatni element to: ", ostatniElementTd);
+
+const pierwszyTd = document.querySelector("td");
+console.log("Pierwszy element to: ", pierwszyTd);
+
