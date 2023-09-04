c71e559b46fef050bd95535389aca3cf944fd688
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-04 22:22:48 +0200

    changes

--- powtorka/dom/todo/todo.js
+++ powtorka/dom/todo/todo.js
@@ -12,13 +12,30 @@ function zrobElement(nazwa, tekst) {
   return element;
 }
 
+function wyswietlKomunikat(tekst) {
+  const ostrzezenie = zrobElement("div", tekst);
+  for (let index = 0; index < bledy.children.length; index++) {
+    bledy.children[index].remove();
+  }
+  bledy.appendChild(ostrzezenie);
+}
+
 const lista = znajdzElement("ul");
 const dodaj = znajdzElement("button");
 const input = znajdzElement("input[name='rzecz']");
+const bledy = znajdzElement("#bledy");
 
 dodaj.addEventListener("click", () => {
   const rzecz = input.value;
   //   console.log(rzecz)
-  const elLi = zrobElement("li", rzecz);
-  lista.appendChild(elLi);
+  input.value = "";
+  if (rzecz !== "") {
+    const elLi = zrobElement("li", rzecz);
+    lista.appendChild(elLi);
+    eleUsun = zrobElement("button", "usun");
+    elLi.appendChild(eleUsun);
+    wyswietlKomunikat("Dodano czynnność");
+  } else {
+    wyswietlKomunikat("Nie można dodać pustej czynności!");
+  }
 });
