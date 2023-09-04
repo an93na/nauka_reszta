f2a4b8e4177542c6cee9e01ef5ec37cbeaf2bfe7
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-04 21:31:29 +0200

    changes

--- powtorka/dom/todo/todo.js
+++ powtorka/dom/todo/todo.js
@@ -6,11 +6,19 @@ function znajdzElement(selektor) {
   return document.querySelector(selektor);
 }
 
+function zrobElement(nazwa, tekst) {
+  const element = document.createElement(nazwa);
+  element.innerText = tekst;
+  return element;
+}
+
 const lista = znajdzElement("ul");
 const dodaj = znajdzElement("button");
 const input = znajdzElement("input[name='rzecz']");
 
 dodaj.addEventListener("click", () => {
   const rzecz = input.value;
-  console.log(rzecz)
+  //   console.log(rzecz)
+  const elLi = zrobElement("li", rzecz);
+  lista.appendChild(elLi);
 });
