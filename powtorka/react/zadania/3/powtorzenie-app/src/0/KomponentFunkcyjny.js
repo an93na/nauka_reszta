c716d16994644eeeb6c2d3ce55ec14ebe6b15225
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-09 20:57:40 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/0/KomponentFunkcyjny.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/0/KomponentFunkcyjny.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export const KomponentFunkcyjny = () => {
   const div = <div>div</div>;
+  const przykladowaZmienna =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis harum omnis in sequi suscipit aut corporis ex culpa non possimus, cumque illum nulla nam, eos cupiditate placeat illo soluta repellat.";
+  const user = { name: "Jan", age: 20 };
+  const tablicaLiczb = [1, 2, 34, 5, 6, 4];
+  const tablicaStringow = ["jakis", "krotki", "string"];
   return (
     <article>
       <h3>Komponent Funkcyjny</h3>
@@ -9,6 +14,17 @@ export const KomponentFunkcyjny = () => {
       <button style={{ width: "auto", padding: "5px", margin: "5px" }}>
         Click me
       </button>
+      <p>{przykladowaZmienna}</p>
+      <ul>
+        <li>
+          <b>Name:</b> {user.name}
+        </li>
+        <li>
+          <b>Age:</b> {user.age}
+        </li>
+      </ul>
+      <div>{tablicaLiczb.slice("").join(", ")}</div>
+      <div>{tablicaStringow.slice("").join(" ")}</div>
     </article>
   );
 };
