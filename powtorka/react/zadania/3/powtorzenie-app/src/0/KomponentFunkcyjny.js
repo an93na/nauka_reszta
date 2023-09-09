31896163c3dee4b323474eb62312a2ae5fea40d3
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-09 20:40:51 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/0/KomponentFunkcyjny.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/0/KomponentFunkcyjny.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 export const KomponentFunkcyjny = () => {
+  const div = <div>div</div>;
   return (
     <article>
       <h3>Komponent Funkcyjny</h3>
+      {div}
+      <button style={{ width: "auto", padding: "5px", margin: "5px" }}>
+        Click me
+      </button>
     </article>
   );
 };
