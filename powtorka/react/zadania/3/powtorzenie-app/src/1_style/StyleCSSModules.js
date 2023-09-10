149950474ed140a4c433f13b9d1a1cbbdcbd47f9
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-10 11:07:53 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/1_style/StyleCSSModules.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/1_style/StyleCSSModules.js
@@ -1,9 +1,12 @@
 import React from "react";
+import classes from'./StyleCSSModule.module.css'
 
 export const StyleCSSModules = () => {
   return (
     <article>
-      <h3>StyleCSSModules</h3>
+      <h3 className={classes.naglowek}>StyleCSSModules</h3>
+      <p className={classes.mojaKlasa}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ea eligendi animi dolores nihil esse odio porro labore facilis laborum reprehenderit neque magni pariatur aliquid deleniti, corporis assumenda tempore a.
+      Laudantium nemo error sequi beatae velit eos assumenda, fugit perferendis harum quas, officia est dolor nostrum possimus! Ipsam repellat saepe debitis hic sapiente suscipit doloremque, veniam id eos beatae minus!</p>
     </article>
   );
 };
