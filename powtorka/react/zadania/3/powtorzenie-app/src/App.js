ae1ae1adde0b8fc9ebc7aa3a537d821ea1ea6d78
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-10 11:30:19 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/App.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/App.js
@@ -3,6 +3,7 @@ import { MyFirstComponent } from "./0_komponent/ex1/ex1";
 import { StyleCSS } from "./1_style/StyleCSS";
 import { StyleCSSModules } from "./1_style/StyleCSSModules";
 import { StyleInline } from "./1_style/StyleInline";
+import { StyleCSSExercise } from "./1_style/ex1/StyleCSSExercise";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <StyleInline/>
       <StyleCSS/>
       <StyleCSSModules/>
+      <StyleCSSExercise/>
     </div>
   );
 }
