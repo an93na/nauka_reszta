43edc326908a6bbacc5c2eae406df1c3360a4218
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-10 18:51:12 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/1_style/ex3/StyleInlineExercise.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/1_style/ex3/StyleInlineExercise.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export const StyleInlineExercise = () => {
   return (
-    <article>
+    <article style={{verticalAlign: "bottom"}}>
       <p> Style Inline Exercise</p>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+      <div style={{backgroundColor:'blue', borderRadius: '50%', height: '40px', width: '40px', display: 'inline-block', textAlign: 'center', lineHeight: '40px'}}>1</div>
+      <div style={{backgroundColor:'green', borderRadius: '50%', height: '60px', width: '60px', display: 'inline-block', textAlign: 'center', lineHeight: '60px'}}>2</div>
+      <div style={{backgroundColor:'red', borderRadius: '50%', height: '80px', width: '80px', display: 'inline-block', textAlign: 'center', lineHeight: '80px'}}>3</div>
     </article>
   );
 };
