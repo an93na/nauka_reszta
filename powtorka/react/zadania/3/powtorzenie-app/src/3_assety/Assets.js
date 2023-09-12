07ea49ff51ae164f18878516113933892f21fb2b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-12 20:41:02 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/3_assety/Assets.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/3_assety/Assets.js
@@ -1,8 +1,21 @@
 import React from "react";
-
+import cacekImg from "./cacek.jpg";
+import gacekImg from "./gacek.jpg";
 
 export const Assets = () => {
-    return <article>
-        <h3>Assets</h3>
+  return (
+    <article>
+      <h3>Assets</h3>
+      <img
+        src={cacekImg}
+        alt="cacek"
+        style={{ width: "200px", padding: "5px", borderRadius: "25px" }}
+      />
+      <img
+        src={gacekImg}
+        alt="gacek"
+        style={{ width: "200px", padding: "5px", borderRadius: "25px" }}
+      />
     </article>
-}
+  );
+};
