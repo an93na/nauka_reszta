4df744a8068a5bd811c039718273cb9d46c0a079
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-13 19:09:24 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/4_stanKomponentu/StanKomponentu.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/4_stanKomponentu/StanKomponentu.js
@@ -24,15 +24,32 @@ export const StanKomponentu = () => {
   const [number, setNumber] = useState(0);
   const [name, setName] = useState("Joe");
   const [lastName, setLastName] = useState("Doe");
+  const [age, setAge] = useState(25);
+  const [address, setAddress] = useState({
+    city: "Warsaw",
+    street: "Dworcowa",
+    postalCode: 123,
+  });
+
   //   setTimeout(() => {
   //     setNumber(number + 1);
   //   }, 1000);
+
+  setTimeout(() => {
+    // const newAddress = { city: "Cracow", street: "Dluga", postalCode: 29 };
+    // setAddress(newAddress);
+    setAddress((prevAddress) => ({ ...prevAddress, street: "Kwiatowa" }));
+  }, 5000);
   return (
     <article>
       <h3>StanKomponentu</h3>
       <p>state: {number}</p>
       <p>Name: {name}</p>
       <p>Last Name: {lastName}</p>
+      <p>Age: {age}</p>
+      <p>City: {address.city}</p>
+      <p>Street: {address.street}</p>
+      <p>Postal Code: {address.postalCode}</p>
     </article>
   );
 };
