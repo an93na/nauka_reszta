344505ddebc81f1357ed316747114aeb91c77769
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-16 20:40:59 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/6_parametryFunkcji/ParametryFunkcji.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/6_parametryFunkcji/ParametryFunkcji.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+
+const ComponentWithProps = () => {
+  const number1 = 10;
+  const number2 = 10;
+  return <div>Suma dwóch liczb: {number1 + number2}</div>
+};
 
 export const ParametryFunkcji = () => {
-    return <article>
-        <h3>Parametry Funkcji</h3>
+  return (
+    <article>
+      <h3>Parametry Funkcji</h3>
+      <ComponentWithProps/>
+      <ComponentWithProps/>
+      <ComponentWithProps/>
     </article>
-}
+  );
+};
