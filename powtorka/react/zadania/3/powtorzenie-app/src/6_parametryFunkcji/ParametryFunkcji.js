321f8d7ef2d6e09a793f0e33fcaa5ad80c306ccc
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-17 20:34:10 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/6_parametryFunkcji/ParametryFunkcji.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/6_parametryFunkcji/ParametryFunkcji.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ComponentWithProps = (props) => {
   const { number1, number2 } = props;
@@ -15,18 +15,35 @@ const UsersList = (props) => {
 };
 
 const CounterControler = (props) => {
-
-}
+  return (
+    <div style={{ display: "flex", gap: 20 }}>
+      <button onClick={() => props.funkcjaZmieniająca(props.wynik + 1)}>
+        +1
+      </button>
+      <button onClick={() => props.funkcjaZmieniająca(props.wynik - 1)}>
+        -1
+      </button>
+      <button onClick={() => props.funkcjaZmieniająca(props.wynik + 5)}>
+        +5
+      </button>
+      <button onClick={() => props.funkcjaZmieniająca(props.wynik - 5)}>
+        -5
+      </button>
+      <button onClick={() => props.funkcjaZmieniająca(0)}>RESET</button>
+    </div>
+  );
+};
 
 const CounterOutput = (props) => {
-
-}
+  return <p>Wartość Countera: {props.wynik}</p>;
+};
 
 export const ParametryFunkcji = () => {
   const users = [
     { name: "John", lastName: "Doe" },
     { name: "Jane", lastName: "Doe" },
   ];
+  const [counter, setCounter] = useState(0);
   return (
     <article>
       <h3>Parametry Funkcji</h3>
@@ -34,8 +51,8 @@ export const ParametryFunkcji = () => {
       <ul>
         <UsersList name={users[0].name} lastName={users[0].lastName} />
       </ul>
-      <CounterControler/>
-      <CounterOutput/>
+      <CounterControler wynik={counter} funkcjaZmieniająca={setCounter} />
+      <CounterOutput wynik={counter} />
     </article>
   );
 };
