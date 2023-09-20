960b1f8ce3dfb6f2fd0acfe60a495cf0fee47874
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-20 19:04:02 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/8_wyswietlanieWarunkowe/WyswietlanieWarunkowe.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/8_wyswietlanieWarunkowe/WyswietlanieWarunkowe.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+
+const Component1 = () => {
+  return <div style={{ color: "blue" }}>Component1</div>;
+};
+const Component2 = () => {
+  return <div style={{ color: "green" }}>Component2</div>;
+};
+const Component3 = () => {
+  return <div style={{ color: "red" }}>Component3</div>;
+};
 
 export const WyswietlanieWarunkowe = () => {
-    return <article>
-        <h3>Wyświetlanie Warunkowe</h3>
+  const [isChecked, setChecked] = useState(false);
+  const [cmp, setCmp] = useState("");
+  if (isChecked) {
+    return (
+      <div>
+        odblokowałem się
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          checked={isChecked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+      </div>
+    );
+  }
+  return (
+    <article>
+      <h3>Wyświetlanie Warunkowe</h3>
+      <label>
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          checked={isChecked}
+          //   onChange={(e) => setChecked(e.target.checked)}
+          onChange={() => setChecked((prevState) => !prevState)}
+        />
+        wyświetl
+      </label>
+      {isChecked && <p> Lorem ipsum dolor sit amet.</p>}
+      {isChecked ? <p>Checkbox zaznaczony</p> : <p>Checkobox nie zaznaczony</p>}
+
+      <select
+        name=""
+        id=""
+        value={cmp}
+        onChange={(e) => setCmp(e.target.value)}
+      >
+        <option value="" hidden>
+          Wybierz komponent
+        </option>
+        <option value="cmp1">Component1</option>
+        <option value="cmp2">Component2</option>
+        <option value="cmp3">Component3</option>
+      </select>
+      {cmp === "cmp1" && <Component1 />}
+      {cmp === "cmp2" && <Component2 />}
+      {cmp === "cmp3" && <Component3 />}
     </article>
-}
+  );
+};
