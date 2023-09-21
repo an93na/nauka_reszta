eae0cf49bb4326bdf7a2b1786352292a025483cd
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-21 18:09:32 +0200

    changes

---
+++ powtorka/react/zadania/3/powtorzenie-app/src/8_wyswietlanieWarunkowe/ex3/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h5>Home</h5>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo maxime
+        sint deserunt ratione ex id dolorem dolor fugit, cum voluptas ad dolore.
+        Fugiat quisquam cupiditate officiis in dolores nam iure. Modi animi
+        dolorem aut reprehenderit minima tempore iure, reiciendis debitis
+        architecto inventore explicabo maxime molestias voluptatem expedita
+        fugit laudantium ducimus ut pariatur in magni obcaecati accusantium, eos
+        commodi. Magnam, quasi?
+      </p>
+    </div>
+  );
+};
