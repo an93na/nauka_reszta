8325a74e2323955d128733ebdb309e6a9417e2a2
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-26 20:02:03 +0200

    changes

--- powtorka/react/zadania/3/powtorzenie-app/src/9_cyklZyciaKomponentu/CyklZyciaKomponentu.js
+++ powtorka/react/zadania/3/powtorzenie-app/src/9_cyklZyciaKomponentu/CyklZyciaKomponentu.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Form } from "./components/Form";
 import { Home } from "./components/Home";
 import { List } from "./components/List";
+import { Users } from "./components/Users";
 
 const style = {
   margin: 20,
@@ -52,12 +53,23 @@ export const CyklZyciaKomponentow = () => {
           />
           List
         </label>
+        <label>
+          <input
+            checked={navigationState === "users"}
+            value="users"
+            type="radio"
+            name="nav"
+            onChange={handleChange}
+          />
+          Users
+        </label>
       </div>
 
       <div style={{ margin: 20, marginTop: 50 }}>
         {navigationState === "home" && <Home />}
         {navigationState === "form" && <Form />}
         {navigationState === "list" && <List />}
+        {navigationState === 'users' && <Users/>}
       </div>
     </article>
   );
