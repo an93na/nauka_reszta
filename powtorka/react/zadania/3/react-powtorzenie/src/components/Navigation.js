1c7dd16f28ab8b9a1ce199295049cb23e7d20d9f
Anna Giermakowska <an.giermakowska@gmail.com>
2023-09-30 18:06:46 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/Navigation.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/Navigation.js
@@ -8,6 +8,7 @@ export const Navigation = (props) => {
       <Link to="form">Form</Link>
       <Link to="list">List</Link>
       <Link to="about">About</Link>
+      <Link to='todo'>Todo</Link>
     </div>
   );
 };
