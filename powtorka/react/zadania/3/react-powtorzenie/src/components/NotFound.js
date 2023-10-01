0d803830c78ff6f95dc7933c20ade0c445386209
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-01 21:16:14 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/NotFound.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/NotFound.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export const NotFound = () => {
   return (
     <div>
-        <img src="https://httpstatusdogs.com/img/404.jpg" alt="404" />
+      <img src="https://httpstatusdogs.com/img/404.jpg" alt="404" />
     </div>
-  )
-}
+  );
+};
