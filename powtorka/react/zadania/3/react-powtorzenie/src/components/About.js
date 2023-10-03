e963d20890e8ee57f7661c06b9db4d98f18e7640
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-03 20:34:28 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/About.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/About.js
@@ -7,6 +7,11 @@ export const About = () => {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
         quisquam.
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+        error a aliquid voluptas ab natus optio quos eius! Dolorum dolores quos
+        totam? Aut quaerat assumenda totam earum pariatur sapiente id.
+      </p>
     </div>
   );
 };
