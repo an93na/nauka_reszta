9045f9807013ef9571d650728e2f707600a8ad0d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-04 20:02:49 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/Home.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/Home.js
@@ -12,6 +12,20 @@ export const Home = () => {
       Autem nesciunt consequatur ducimus beatae, eveniet ab in eos facere placeat veritatis magni nihil repellendus, expedita aut, architecto hic? Cumque magnam quae eius rerum eos quisquam consequuntur iure maiores nesciunt.
       </p>
     </div>
+    <div>
+      <div style={{width:'70px', height:'70px', backgroundColor: 'red', borderRadius: '50%', float: 'right', padding: '10px', margin: '5px'}}></div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla culpa molestiae quaerat repellat enim deleniti similique voluptatibus inventore, aperiam iusto, odit rerum rem nihil autem quisquam officiis, adipisci explicabo. Repellat?
+      Molestias soluta veniam natus! Qui expedita, quod exercitationem, totam corporis itaque rem culpa aperiam nam labore quas repellat vel a amet quaerat esse est maxime impedit nemo sit cumque nihil!
+      Autem nesciunt consequatur ducimus beatae, eveniet ab in eos facere placeat veritatis magni nihil repellendus, expedita aut, architecto hic? Cumque magnam quae eius rerum eos quisquam consequuntur iure maiores nesciunt.
+      </p>
+    </div>
+    <div>
+      <div style={{width:'70px', height:'70px', backgroundColor: 'blue', borderRadius: '50%', float: 'none', padding: '10px', margin: '5px'}}></div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla culpa molestiae quaerat repellat enim deleniti similique voluptatibus inventore, aperiam iusto, odit rerum rem nihil autem quisquam officiis, adipisci explicabo. Repellat?
+      Molestias soluta veniam natus! Qui expedita, quod exercitationem, totam corporis itaque rem culpa aperiam nam labore quas repellat vel a amet quaerat esse est maxime impedit nemo sit cumque nihil!
+      Autem nesciunt consequatur ducimus beatae, eveniet ab in eos facere placeat veritatis magni nihil repellendus, expedita aut, architecto hic? Cumque magnam quae eius rerum eos quisquam consequuntur iure maiores nesciunt.
+      </p>
+    </div>
     </div>
   );
 };
