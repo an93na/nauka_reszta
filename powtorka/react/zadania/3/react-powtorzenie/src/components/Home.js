a1beac6198c8fc290d3b1f2594a032110781534b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-04 19:58:49 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/Home.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/Home.js
@@ -6,7 +6,7 @@ export const Home = () => {
       <h2>Todo lista</h2>
       <p>Aplikacja do zarządzania i wyświetlania zadaniami do zrobienia</p>
     <div>
-      <div></div>
+      <div style={{width:'70px', height:'70px', backgroundColor: 'green'}}></div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla culpa molestiae quaerat repellat enim deleniti similique voluptatibus inventore, aperiam iusto, odit rerum rem nihil autem quisquam officiis, adipisci explicabo. Repellat?
       Molestias soluta veniam natus! Qui expedita, quod exercitationem, totam corporis itaque rem culpa aperiam nam labore quas repellat vel a amet quaerat esse est maxime impedit nemo sit cumque nihil!
       Autem nesciunt consequatur ducimus beatae, eveniet ab in eos facere placeat veritatis magni nihil repellendus, expedita aut, architecto hic? Cumque magnam quae eius rerum eos quisquam consequuntur iure maiores nesciunt.
