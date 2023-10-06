2a3543f5d52cf4704a8dbca612c1808c54c33263
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-06 20:44:34 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/About.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/About.js
@@ -4,15 +4,23 @@ export const About = () => {
   return (
     <div>
       <p>
-        <b style={{color:'green'}}>Lorem ipsum</b>  dolor sit amet, consectetur adipisicing elit. Nihil,
-        quisquam.
+        <b style={{ color: "green" }}>Lorem ipsum</b> dolor sit amet,
+        consectetur adipisicing elit. Nihil, quisquam.
       </p>
       <p>
-        <b style={{color:'red'}}>Lorem ipsum</b> dolor sit amet consectetur, adipisicing elit. Voluptatibus
-        error a aliquid voluptas ab natus optio quos eius! Dolorum dolores quos
-        totam? Aut quaerat assumenda totam earum pariatur sapiente id.
+        <b style={{ color: "red" }}>Lorem ipsum</b> dolor sit amet consectetur,
+        adipisicing elit. Voluptatibus error a aliquid voluptas ab natus optio
+        quos eius! Dolorum dolores quos totam? Aut quaerat assumenda totam earum
+        pariatur sapiente id.
       </p>
-      <div style={{width: 80, height: 80, backgroundColor: 'palevioletred', borderRadius: '50%'}}></div>
+      <div
+        style={{
+          width: 80,
+          height: 80,
+          backgroundColor: "palevioletred",
+          borderRadius: "50%",
+        }}
+      ></div>
     </div>
   );
 };
