d7e8fff7fd212111a78e76d76188c5ba3a8a0a90
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-06 20:43:58 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/Todo.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/Todo.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <div>
+      Todo
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit obcaecati
+        voluptates ipsum totam officiis, veniam eaque nulla consequuntur eum
+        quasi quia itaque accusantium numquam accusamus, aut fugiat distinctio
+        in dolores.
+      </p>
+    </div>
+  );
 };
