92ad656dfa01f600676bb8245c4d7feb230c70e2
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-07 22:13:25 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/About.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/About.js
@@ -11,8 +11,10 @@ export const About = () => {
         <b style={{ color: "red" }}>Lorem ipsum</b> dolor sit amet consectetur,
         adipisicing elit. Voluptatibus error a aliquid voluptas ab natus optio
         quos eius! Dolorum dolores quos totam? Aut quaerat assumenda totam earum
-        pariatur sapiente id.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium at magnam sunt doloremque id esse numquam eaque, laborum mollitia voluptate voluptatibus ex quia voluptas fuga, maiores quo rerum totam minus?
+        pariatur sapiente id. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Laudantium at magnam sunt doloremque id esse numquam eaque,
+        laborum mollitia voluptate voluptatibus ex quia voluptas fuga, maiores
+        quo rerum totam minus?
       </p>
       <div
         style={{
