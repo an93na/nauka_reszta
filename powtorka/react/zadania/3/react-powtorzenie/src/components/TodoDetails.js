bb76ca8166fce5a4e7dae9d0cfdca109c0620e86
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-08 20:43:01 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/TodoDetails.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/TodoDetails.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 export const TodoDetails = () => {
-    const {id} = useParams()
-  return (
-    <h2>Todo title: {id}</h2>
-  )
-}
+  const { id } = useParams();
+  return <h2>Todo title: {id}</h2>;
+};
