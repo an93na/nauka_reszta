c78c07c3ea70cc3a79a57b437bdc23e4475c180e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-09 18:45:25 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/ExerciseApp.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/ExerciseApp.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Form } from './components/Form';
+import { List } from './components/List';
+import { NotFound } from './components/NotFound';
+import { Users } from './components/Users';
 
 function ExerciseApp() {
+    const style = {
+        margin: 20,
+        padding: 20,
+        border: "2px solid blue",
+        display: "flex",
+        borderRadius: 5,
+        gap: 40,
+      };
+
     return <div className="container">
-        
+        <article>
+            <div style={style}>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/form'}>Form</Link>
+            <Link to={'/list'}>List</Link>
+            <Link to={'/users'}>Users</Link>
+            </div>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/form' element={<Form/>}/>
+                <Route path='/list' element={<List/>}/>
+                <Route path='/users' element={<Users/>}/>
+                <Route path='/*' element={<NotFound/>}/>
+            </Routes>
+        </article>
     </div>;
     
   }
