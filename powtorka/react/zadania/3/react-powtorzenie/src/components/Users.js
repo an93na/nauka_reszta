36935a6f701cfb8720d53f3720589c889c627596
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-10 19:41:11 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/Users.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/Users.js
@@ -1,23 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Users = () => {
-    const [users, setUsers] = useState([]);
-    
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3010/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div>
       <h3>Users</h3>
-     <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>LastName</th>
-          <th>Age</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-      </tbody>
-     </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>LastName</th>
+            <th>Age</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   );
 };
