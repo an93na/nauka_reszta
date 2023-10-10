0f4e963ea0ad4eb6203c04896004add4d5dbc307
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-10 19:37:22 +0200

    changes

--- powtorka/react/zadania/3/react-powtorzenie/src/components/Users.js
+++ powtorka/react/zadania/3/react-powtorzenie/src/components/Users.js
@@ -2,9 +2,22 @@ import React, { useState } from "react";
 
 export const Users = () => {
     const [users, setUsers] = useState([]);
+    
   return (
     <div>
       <h3>Users</h3>
+     <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>LastName</th>
+          <th>Age</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+     </table>
     </div>
   );
 };
