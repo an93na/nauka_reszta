508cc5a59d1eb819b7570a95bd88f7c17c0285c0
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-14 16:41:51 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/1_pow/Decrease.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/1_pow/Decrease.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Decrease = (props) => {
   const { counter, setCounter } = props;
   return (
-    <div style={{display: 'flex', gap: 20}}>
+    <div style={{ display: "flex", gap: 20 }}>
       <button onClick={() => setCounter(counter - 1)}>-1</button>
       <button onClick={() => setCounter(counter - 5)}>-5</button>
     </div>
