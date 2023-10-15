6e14069fd15a420deba85e6a94ab85ba7670d260
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-15 20:05:04 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/store/store.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../1_pow/counterSlice";
+import counterReducer2 from "../1_pow/counterSlice2";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    counter2: counterReducer2,
   },
 });
