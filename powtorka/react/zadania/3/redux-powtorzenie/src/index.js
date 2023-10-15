aa1b1cb03f5b228f1144cee301cd1c2ae9b29873
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-15 19:57:34 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/index.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "@picocss/pico";
 import { BrowserRouter } from "react-router-dom";
-
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import "@picocss/pico";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
