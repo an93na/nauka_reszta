9e00a16995e4f85932433727148cee19c0577185
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-16 22:07:00 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/conterSlice.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/conterSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const counterSlice = createSlice({
+const counterSlice = createSlice({
     name: 'counter3',
     initialState: {
         value: 0
     },
     reducers: {}
 })
+
+export default counterSlice.reducer
--- powtorka/react/zadania/3/redux-powtorzenie/src/store/store.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/store/store.js
@@ -1,15 +1,17 @@
 // store trzyma globalny stan aplikacji
-//  a plik z reducer czyli counterSlice to taki plik 
+//  a plik z reducer czyli counterSlice to taki plik
 //  z kawałkami stanu
 
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../1_pow/counterSlice";
 import counterReducer2 from "../1_pow/counterSlice2";
+import counterReducer3 from "../2_rexCon/conterSlice";
 
 export const store = configureStore({
   // reducer zbiera nam te kawałki stanu
   reducer: {
     counter: counterReducer,
     counter2: counterReducer2,
+    counter3: counterReducer3,
   },
 });
