34f084aeaecd71006c11af0a9745a11b1c597c31
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-16 22:04:04 +0200

    changes

---
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/conterSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+    name: 'counter3',
+    initialState: {
+        value: 0
+    },
+    reducers: {}
+})
