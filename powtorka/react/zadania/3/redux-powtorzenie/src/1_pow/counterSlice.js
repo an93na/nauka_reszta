0113c97f9f6352b0558d91d590a25289837567fb
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-16 21:40:59 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/1_pow/counterSlice.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/1_pow/counterSlice.js
@@ -1,10 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const counterSlice = createSlice({
+  // stan
   name: "counter",
   initialState: {
     value: 200,
   },
+  // metody do stanu, jakie funkcje będą mogły 
+  // wpływać na stan
   reducers: {},
 });
 
