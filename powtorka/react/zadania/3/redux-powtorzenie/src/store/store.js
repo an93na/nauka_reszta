c8e5f9f4e2e8470866710d105b8a360e96f325a5
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-16 18:39:47 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/store/store.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/store/store.js
@@ -1,3 +1,7 @@
+// store trzyma globalny stan aplikacji
+//  a plik z reducer czyli counterSlice to taki plik 
+//  z kawałkami stanu
+
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../1_pow/counterSlice";
 import counterReducer2 from "../1_pow/counterSlice2";
