3fc090e8116e4a5e55583d09aefa280fff1af6a2
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-17 20:03:37 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/Counter.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/Counter.js
@@ -2,10 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export const Counter = () => {
-    const counter = useSelector((state) => state.counter.value)
-  return <article>
-    <button>+1</button>
-    <input type="text" value={counter} readOnly />
-    <button>-1</button>
-    </article>;
+  const counter = useSelector((state) => state.counter.value);
+  return (
+    <article>
+      <button>+1</button>
+      <input type="text" value={counter} readOnly />
+      <button>-1</button>
+    </article>
+  );
 };
