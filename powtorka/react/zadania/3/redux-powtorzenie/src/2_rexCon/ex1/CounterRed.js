3b98a68df28d89251f83087d3807542a6d1b0559
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-18 21:02:20 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex1/CounterRed.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex1/CounterRed.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "./conterSliceEx";
 
 export const CounterRed = () => {
-    const counter = useSelector((state) => state.counter4.value)
+  const counter = useSelector((state) => state.counter4.value);
+  const dispatch = useDispatch();
   return (
-    <article>{counter}</article>
-  )
-}
+    <article>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +5
+      </button>
+      <input type="text" value={counter} readOnly />
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        -5
+      </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        RESET
+      </button>
+    </article>
+  );
+};
--- powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex1/conterSliceEx.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex1/conterSliceEx.js
@@ -3,17 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 const counterSliceEx = createSlice({
   name: "counter4",
   initialState: {
-    value: 20,
+    value: 0,
   },
   reducers: {
     increment: (state) => {
-      state.value = state.value + 1;
+      state.value = state.value + 5;
     },
     decrement: (state) => {
-      state.value = state.value - 1;
+      state.value = state.value - 5;
+    },
+    reset: (state) => {
+      state.value = 0;
     },
   },
 });
 
-export const { increment, decrement } = counterSliceEx.actions;
+export const { increment, decrement, reset } = counterSliceEx.actions;
 export default counterSliceEx.reducer;
