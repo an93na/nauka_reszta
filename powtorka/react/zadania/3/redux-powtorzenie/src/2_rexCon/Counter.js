c3ca9dd181a94092580ccc70b059f79fff150a57
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-18 20:07:53 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/Counter.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/Counter.js
@@ -1,21 +1,43 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement, resetCounter, toggleShowPoweredValue } from "./conterSlice";
+import {
+  increment,
+  decrement,
+  resetCounter,
+  toggleShowPoweredValue,
+} from "./conterSlice";
 
 export const Counter = () => {
   const counter = useSelector((state) => state.counter3.value);
-  const dispatch = useDispatch()
-  const showPoweredValue = useSelector((state) => state.counter3.showPoweredValue)
+  const dispatch = useDispatch();
+  const showPoweredValue = useSelector(
+    (state) => state.counter3.showPoweredValue
+  );
   return (
     <article>
       <button onClick={() => dispatch(increment())}>+1</button>
       <input type="text" value={counter} readOnly />
       <button onClick={() => dispatch(decrement())}>-1</button>
       <button onClick={() => dispatch(resetCounter())}>Reset</button>
-    <label>
+      <label>
         Show powered value
-        <input type="checkbox" name="" id="" checked={showPoweredValue} onChange={()=>{dispatch(toggleShowPoweredValue())}}/>
-    </label>
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          checked={showPoweredValue}
+          onChange={() => {
+            dispatch(toggleShowPoweredValue());
+          }}
+        />
+      </label>
+      {showPoweredValue && (
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis a
+          unde quo pariatur hic deleniti nam temporibus dignissimos nobis.
+          Voluptas aliquam unde quia, cumque dolorum labore iure dicta eius eum.
+        </p>
+      )}
     </article>
   );
 };
