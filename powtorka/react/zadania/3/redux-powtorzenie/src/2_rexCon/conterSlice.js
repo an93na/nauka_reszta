7503e8e23ec13ee573d09ffb14ea573fe26d11ed
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-18 20:19:22 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/conterSlice.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/conterSlice.js
@@ -5,24 +5,28 @@ const counterSlice = createSlice({
   initialState: {
     value: 0,
     poweredValue: 0,
-    showPoweredValue: false
+    showPoweredValue: false,
   },
   reducers: {
     increment: (state) => {
       state.value = state.value + 1;
+      state.poweredValue = state.value * state.value;
     },
     decrement: (state) => {
       state.value = state.value - 1;
+      state.poweredValue = state.value * state.value;
     },
     resetCounter: (state) => {
       state.value = 0;
+      state.poweredValue = 0;
     },
     toggleShowPoweredValue: (state) => {
-        state.showPoweredValue = !state.showPoweredValue
-    }
+      state.showPoweredValue = !state.showPoweredValue;
+    },
   },
 });
 
-export const { decrement, increment, resetCounter, toggleShowPoweredValue } = counterSlice.actions;
+export const { decrement, increment, resetCounter, toggleShowPoweredValue } =
+  counterSlice.actions;
 
 export default counterSlice.reducer;
