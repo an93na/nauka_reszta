1b2635e23d9a65223e6f7a79dbc059f5af0aa2c2
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-20 19:40:03 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex3/CounterEx3.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex3/CounterEx3.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Increase } from "./Increase";
+import { Decrease } from "./Decrease";
+import { Output } from "./Output";
 
 export const CounterEx3 = () => {
   return (
-    <article>CounterEx3</article>
-  )
-}
+    <article>
+      <h3>Complex Counter</h3>
+      <div style={{ display: "flex", gap: 20 }}>
+        <Increase value={5} />
+        <Increase value={1} />
+        <Output />
+        <Decrease value={1} />
+        <Decrease value={5} />
+      </div>
+    </article>
+  );
+};
