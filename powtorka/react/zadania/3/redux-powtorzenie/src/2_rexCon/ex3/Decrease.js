dc433d9a1d892dd22d9843d0ce6da9fff1f09340
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-20 19:36:48 +0200

    changes

---
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex3/Decrease.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Decrease = () => {
+  return (
+    <div>Decrease</div>
+  )
+}
--- powtorka/react/zadania/3/redux-powtorzenie/src/App.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/App.js
@@ -2,6 +2,7 @@ import { Counter } from "./1_pow/Counter";
 import { Counter as Counter2 } from "./2_rexCon/Counter";
 import { CounterRed } from "./2_rexCon/ex1/CounterRed";
 import { Counter3 } from "./2_rexCon/ex2/Counter3";
+import { CounterEx3 } from "./2_rexCon/ex3/CounterEx3";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Counter2 />
       <CounterRed />
       <Counter3 />
+      <CounterEx3/>
     </div>
   );
 }
