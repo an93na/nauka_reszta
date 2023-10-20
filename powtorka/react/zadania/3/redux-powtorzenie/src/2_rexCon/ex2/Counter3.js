c22bf2874fe960d0fe59be28d55cb08c066adafd
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-20 18:08:22 +0200

    changes

---
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex2/Counter3.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Counter3 = () => {
+  return (
+    <div>Counter3</div>
+  )
+}
---
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex2/counterSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const conterSliceEx2 = createSlice({
+  name: "counter5",
+  initialState: {
+    value: 0,
+  },
+  reducers: {},
+});
+
+export default conterSliceEx2.reducer;
