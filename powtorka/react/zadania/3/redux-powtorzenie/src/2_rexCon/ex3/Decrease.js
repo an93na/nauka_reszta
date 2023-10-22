f7510b67620274332f5258283fe8812c39f77f77
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-22 20:46:49 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex3/Decrease.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/2_rexCon/ex3/Decrease.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { decrement } from './counterSliceEx3'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { decrement } from "./counterSliceEx3";
 
 export const Decrease = (props) => {
-    const {value} = props
-    const dispatch = useDispatch()
+  const { value } = props;
+  const dispatch = useDispatch();
   return (
-    <button onClick={() => dispatch(decrement(value))} style={{backgroundColor: 'palevioletred'}}>{-value}</button>
-  )
-}
+    <button
+      onClick={() => dispatch(decrement(value))}
+      style={{ backgroundColor: "palevioletred" }}
+    >
+      {-value}
+    </button>
+  );
+};
