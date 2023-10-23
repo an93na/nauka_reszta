93b84c135a54c4e3fc777758481ed7c56d0d0847
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-23 18:19:20 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/3_Todo/TodoList.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/3_Todo/TodoList.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TodoList = () => {
+  const [todoTitle, setTodoTitle] = useState("");
   return (
     <article>
       <h3>TodoList</h3>
-      <form action="">
-        <input type="text" name="" id="" />
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          name=""
+          id=""
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
-      <ul></ul>
+      <ul>{}</ul>
     </article>
   );
 };
