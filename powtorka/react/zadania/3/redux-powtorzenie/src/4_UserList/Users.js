32494b3e6f8f70b173910f253f31e74d7e477fc7
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-26 21:08:49 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/Users.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/Users.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { UserForm } from "./UserForm";
+import { Filters } from "./Filters";
+import { Metric } from "./Metric";
+import { UsersList } from "./UsersList";
 
 export const Users = () => {
   return (
     <article>
       <h3>Users</h3>
+      <UserForm/>
+      <Filters/>
+      <Metric />
+      <UsersList />
     </article>
   );
 };
