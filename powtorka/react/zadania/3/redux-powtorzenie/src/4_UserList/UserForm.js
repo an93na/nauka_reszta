75223ae2cae09ef46e7c7c0a439cd6683a4e3c3a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-27 19:56:09 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/UserForm.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/UserForm.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export const UserForm = () => {
   return (
-    <form>UserForm</form>
-  )
-}
+    <form>
+      <input type="text" name="name" placeholder="Name" required />
+      <input type="text" name="lastName" placeholder="LastName" required />
+      <input type="text" name="age" placeholder="Age" required />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
