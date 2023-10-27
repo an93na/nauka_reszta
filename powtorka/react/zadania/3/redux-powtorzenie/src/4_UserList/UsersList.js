9463039a6c4f02603732fcb23b1c4372c24eb8be
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-27 19:45:30 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/UsersList.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/UsersList.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { usersSelector } from './usersSlice'
+import React from "react";
+import { useSelector } from "react-redux";
+import { usersSelector } from "./usersSlice";
 
 export const UsersList = () => {
   const selectUser = useSelector(usersSelector);
   return (
     <div>
-      <ul>
-        {selectUser.map((user) => <li key={user.id}>{user.name}</li>)}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Name:</th>
+            <th>LastName:</th>
+            <th>Age:</th>
+          </tr>
+        </thead>
+        <tbody>
+          {selectUser.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.lastName}</td>
+              <td>{user.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
