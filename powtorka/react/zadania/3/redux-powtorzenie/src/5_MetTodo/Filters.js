aa6e521b1c24cb5a60b07f7e7e6101c7c44f05a6
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-28 13:20:28 +0200

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/5_MetTodo/Filters.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/5_MetTodo/Filters.js
@@ -4,10 +4,12 @@ export const Filters = (props) => {
   const { filter, setFilter } = props;
   return (
     <div>
-      <select name="" id="">
-        <option value="" hidden>
-          Wybierz
-        </option>
+      <select
+        name=""
+        id=""
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      >
         <option value="0">string z 3 znakami</option>
         <option value="1">string z wiekszą ilością znamków niż 3</option>
         <option value="2">string z mniejszą ilością znaków niż 3</option>
--- powtorka/react/zadania/3/redux-powtorzenie/src/5_MetTodo/TodoList.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/5_MetTodo/TodoList.js
@@ -3,13 +3,36 @@ import { Filters } from "./Filters";
 import { List } from "./List";
 
 export const TodoList2 = () => {
-  const [todos, setTodos] = useState(["dsds", "dssadeqwwqe", "dswr", "dswqrd"]);
-  const [filter, setFilter] = useState('3')
+  const [todos, setTodos] = useState([
+    "dsds",
+    "eee",
+    "e",
+    "rr",
+    "dssadeqwwqe",
+    "dswr",
+    "dswqrd",
+  ]);
+  const [filter, setFilter] = useState("3");
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "0" && todo.length === 3) {
+      return true;
+    }
+    if (filter === "1" && todo.length > 3) {
+      return true;
+    }
+    if (filter === "2" && todo.length < 3) {
+      return true;
+    }
+    if (filter === "3") {
+      return true;
+    }
+    return false;
+  });
   return (
     <article>
       <p>TodoList</p>
-      <Filters filter={filter} setFilter={setFilter}/>
-      <List todos={todos} />
+      <Filters filter={filter} setFilter={setFilter} />
+      <List todos={filteredTodos} />
     </article>
   );
 };
