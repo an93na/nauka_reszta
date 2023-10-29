f3620e2d3bfbf1a376cb4b6167f6f1207b9eaa47
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-29 16:29:17 +0100

    changes

---
+++ powtorka/react/zadania/3/redux-powtorzenie/src/6_ex6/Todo3slice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos3: [],
+};
+
+const Todo3Slice = createSlice({
+  name: "todos3",
+  initialState: initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos3.push(action.payload);
+    },
+    resetState: (state) => {
+      //reset całego stanu
+      state = initialState;
+    },
+  },
+});
+
+export const stateTodo3 = (state) => state.todo3.todos3;
+export const { addTodo } = Todo3Slice.actions;
+export default Todo3Slice.reducer;
