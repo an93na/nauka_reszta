813b64b7e176e259e1de5bea11de712a430d4682
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-29 16:29:27 +0100

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/6_ex6/TodoList3.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/6_ex6/TodoList3.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Form3 } from "./Form3";
+import { List3 } from "./List";
 
 export const TodoList3 = () => {
   return (
     <article>
       <p>TodoList3</p>
-      
+      <Form3/>
+      <List3/>
     </article>
   );
 };
