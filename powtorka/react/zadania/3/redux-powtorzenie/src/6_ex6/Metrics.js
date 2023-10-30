835e01a65b434b6585d165782e453209d971079b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-30 11:06:15 +0100

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/6_ex6/Metrics.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/6_ex6/Metrics.js
@@ -2,10 +2,18 @@ import React from "react";
 
 export const Metrics = () => {
   return (
-    <div style={{border: 'solid 1px white', borderRadius: '10px', padding: 10}}>
-      <p>Total: {5}</p>
-      <p>Ilość wszystkich znaków: {10}</p>
-      <p>Ilość słów, które mają mniej niż 5 znaków: {10}</p>
+    <div
+      style={{ border: "solid 1px white", borderRadius: "10px", padding: 10 }}
+    >
+      <p>
+        Total: <b>{5}</b>
+      </p>
+      <p>
+        Ilość wszystkich znaków: <b>{10}</b>
+      </p>
+      <p>
+        Ilość słów, które mają mniej niż 5 znaków: <b>{10}</b>
+      </p>
     </div>
   );
 };
