449ac9341a5b173ffab9f8bbf857abbdc267beaa
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-31 11:01:23 +0100

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/usersSlice.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/usersSlice.js
@@ -1,18 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  users: [{ name: "Jole", lastName: "Foo", age: 20, id: Math.random() }],
+  searchPhrase: "",
+};
+
 const userSlice = createSlice({
   name: "user",
-  initialState: {
-    users: [{ name: "Jole", lastName: "Foo", age: 20, id: Math.random() }],
-  },
+  initialState: initialState,
   reducers: {
     addUser: (user, action) => {
       user.users.push(action.payload);
     },
     removeUser: (user, action) => {},
-    setFilter: (user, action) => {},
+    setSearchPhrase: (state, action) => {
+      state.searchPhrase = action.payload;
+    },
+    resetState: () => initialState,
   },
 });
 export const usersSelector = (state) => state.userState.users;
+export const selectSearchPhrase = (state) => state.userState.searchPhrase;
 export const { addUser, removeUser, setFilter } = userSlice.actions;
 export default userSlice.reducer;
