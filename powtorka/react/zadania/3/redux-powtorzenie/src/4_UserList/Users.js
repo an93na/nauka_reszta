85121b44745db5fb028500241346e2564481bd34
Anna Giermakowska <an.giermakowska@gmail.com>
2023-10-31 10:22:31 +0100

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/Users.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/Users.js
@@ -4,12 +4,25 @@ import { Filters } from "./Filters";
 import { Metric } from "./Metric";
 import { UsersList } from "./UsersList";
 
+const style = {
+  border: "solid 1px white",
+  borderRadius: "10px",
+  padding: 10,
+  marginBottom: 15,
+};
+
 export const Users = () => {
   return (
     <article>
       <h3>Users</h3>
-      <UserForm />
-      <Filters />
+      <div style={style}>
+        <h6>UserForm</h6>
+        <UserForm />
+      </div>
+      <div style={style}>
+        <h6>Filters</h6>
+        <Filters />
+      </div>
       <Metric />
       <UsersList />
     </article>
