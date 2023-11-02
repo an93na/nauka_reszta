458375d2258611e862ea7fe5aeae76d739b6e287
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-02 17:05:20 +0100

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/Metric.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/Metric.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-export const Metric = ({ label, value }) => {
-  return (
-    <p
-      style={{
+const style = {
         border: "1px solid gray",
         borderRadius: 10,
         padding: 20,
         width: "fit-content",
-      }}
+      }
+
+export const Metric = ({ label, value }) => {
+  return (
+    <p
+      style={style}
     >
       <b>{label}: </b>
       {value}
