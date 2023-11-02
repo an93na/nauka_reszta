569157443d522100537ee1a3fec5e47968f76281
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-02 17:05:40 +0100

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/Metric.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/4_UserList/Metric.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const style = {
-        border: "1px solid gray",
-        borderRadius: 10,
-        padding: 20,
-        width: "fit-content",
-      }
+  border: "1px solid gray",
+  borderRadius: 10,
+  padding: 20,
+  width: "fit-content",
+};
 
 export const Metric = ({ label, value }) => {
   return (
-    <p
-      style={style}
-    >
+    <p style={style}>
       <b>{label}: </b>
       {value}
     </p>
