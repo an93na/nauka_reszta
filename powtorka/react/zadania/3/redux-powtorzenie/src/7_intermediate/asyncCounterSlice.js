4d268b27f926fc1a7b94e1b305494dbdd838250e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-03 12:03:17 +0100

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/7_intermediate/asyncCounterSlice.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/7_intermediate/asyncCounterSlice.js
@@ -1,1 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getCounter = createAsyncThunk(
+  "counter/getCounter",
+  async (params, thunkAPI) => {
+    const response = await fetch("http://localhost:3010/counter");
+    const data = await response.json();
+    thunkAPI.fulfillWithValue(data);
+  }
+);
+
+const asyncCounterSlice = createSlice({
+  name: "asyncSlice",
+  initialState: {
+    value: 0,
+    errorState: "",
+    isLoading: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCounter.fulfilled, (state, action) => {
+      state.value = action.payload;
+    });
+  },
+});
+
+export default asyncCounterSlice.reducer;
