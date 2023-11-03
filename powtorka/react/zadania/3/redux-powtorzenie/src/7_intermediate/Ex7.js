739f0a2da4e35648ebdd86ad2b93a3e9f23c6698
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-03 11:20:25 +0100

    changes

--- powtorka/react/zadania/3/redux-powtorzenie/src/7_intermediate/Ex7.js
+++ powtorka/react/zadania/3/redux-powtorzenie/src/7_intermediate/Ex7.js
@@ -4,6 +4,9 @@ export const Ex7 = () => {
   return (
     <article>
       <h3>Akcje asynchroniczne</h3>
+      <button>+5</button>
+      <input type="text" name="" id="" value={0} readOnly/>
+      <button>-10</button>
     </article>
   );
 };
