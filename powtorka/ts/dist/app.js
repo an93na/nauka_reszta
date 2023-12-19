95f320fb2875b98945c665b3284ad2591ab6e980
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-19 10:51:15 +0100

    changes

--- powtorka/ts/dist/app.js
+++ powtorka/ts/dist/app.js
@@ -4,3 +4,11 @@ age = 49;
 let ageAsString = "dwadzieścia";
 const add = (v1, v2) => v1 + v2;
 console.log(add(5, 4));
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const button = document.querySelector("button");
+const dodaj = (i1, i2) => i1 + i2;
+button.addEventListener("click", () => {
+    const sum = dodaj(Number(input1.value), Number(input2.value));
+    console.log(sum);
+});
--- powtorka/ts/src/app.ts
+++ powtorka/ts/src/app.ts
@@ -8,3 +8,12 @@ let ageAsString: string = "dwadzieścia";
 const add = (v1: number, v2: number) => v1 + v2;
 console.log(add(5, 4));
 
+const input1: HTMLInputElement = document.querySelector("#input1");
+const input2: HTMLInputElement = document.querySelector("#input2");
+const button = document.querySelector("button");
+
+const dodaj = (i1: number, i2: number) => i1 + i2;
+button.addEventListener("click", () => {
+  const sum = dodaj(Number(input1.value), Number(input2.value));
+  console.log(sum);
+});
