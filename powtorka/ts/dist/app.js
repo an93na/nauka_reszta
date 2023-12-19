340aa0fe412c9e6461138db316247eef084ba9a4
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-19 16:50:21 +0100

    changes

--- powtorka/ts/dist/app.js
+++ powtorka/ts/dist/app.js
@@ -1,14 +1,15 @@
-console.log("TypeScript");
+// console.log("TypeScript");
+// let age: number = 29;
+// age = 49;
+// let ageAsString: string = "dwadzieścia";
+// const add = (v1: number, v2: number) => v1 + v2;
+// console.log(add(5, 4));
+// const input1: HTMLInputElement = document.querySelector("#input1");
+// const input2: HTMLInputElement = document.querySelector("#input2");
+// const button = document.querySelector("button");
+// const dodaj = (i1: number, i2: number) => i1 + i2;
+// button.addEventListener("click", () => {
+//   const sum = dodaj(Number(input1.value), Number(input2.value));
+//   console.log('suma z input: ',sum);
+// });
 let age = 29;
-age = 49;
-let ageAsString = "dwadzieścia";
-const add = (v1, v2) => v1 + v2;
-console.log(add(5, 4));
-const input1 = document.querySelector("#input1");
-const input2 = document.querySelector("#input2");
-const button = document.querySelector("button");
-const dodaj = (i1, i2) => i1 + i2;
-button.addEventListener("click", () => {
-    const sum = dodaj(Number(input1.value), Number(input2.value));
-    console.log(sum);
-});
