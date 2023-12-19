128968f2816e9360007cb21314d5c5b013da3669
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-19 10:39:47 +0100

    changes

--- powtorka/ts/dist/app.js
+++ powtorka/ts/dist/app.js
@@ -1,1 +1,6 @@
 console.log("TypeScript");
+let age = 29;
+age = 49;
+let ageAsString = "dwadzieścia";
+const add = (v1, v2) => v1 + v2;
+console.log(add(5, 4));
--- powtorka/ts/src/app.ts
+++ powtorka/ts/src/app.ts
@@ -1,1 +1,10 @@
 console.log("TypeScript");
+
+let age: number = 29;
+age = 49;
+
+let ageAsString: string = "dwadzieścia";
+
+const add = (v1: number, v2: number) => v1 + v2;
+console.log(add(5, 4));
+
