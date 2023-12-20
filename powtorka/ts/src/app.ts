33accfedb9d1adc5b9a61403fc20595113fa07df
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-20 12:21:15 +0100

    chaneges

--- powtorka/ts/src/app.ts
+++ powtorka/ts/src/app.ts
@@ -63,9 +63,24 @@ const tasks: {
 
 const render = () => {
   tasksContainerElement.innerHTML = "";
-  tasks.forEach((task) => {
+  tasks.forEach((task, index) => {
     const taskElement: HTMLElement = document.createElement("li");
-    taskElement.innerText = task.name;
+    const id: string = `task-${index}`;
+    const labelElement: HTMLLabelElement = document.createElement("label");
+    labelElement.innerText = task.name;
+    labelElement.setAttribute("for", id);
+    const checkboxElement: HTMLInputElement = document.createElement("input");
+    checkboxElement.type = "checkbox";
+    checkboxElement.name = task.name;
+    checkboxElement.id = id;
+    checkboxElement.checked = task.done;
+    checkboxElement.addEventListener("change", () => {
+      task.done = !task.done;
+    });
+
+    taskElement.appendChild(labelElement);
+    taskElement.appendChild(checkboxElement);
+
     tasksContainerElement.appendChild(taskElement);
   });
 };
